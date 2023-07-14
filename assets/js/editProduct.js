a9b59bdb70d73bc450a618971edfd4ffd6241c7b
Alexandre Munis <aalexandrm456@gmail.com>
2023-07-14 15:00:06 -0300

    criando funções para dividir o editProduct.js

--- assets/js/editProduct.js
+++ assets/js/editProduct.js
@@ -4,27 +4,26 @@ const urlParams = new URLSearchParams(window.location.search);
 const idUrl = urlParams.get("id");
 const formData = document.querySelector("[data-edit");
 
-async function getProduct() {
-  const product = await connectApi.selectById(idUrl);
-  
+// Cria formulario com os dados do produto
+function createForm(titulo, preco, imagem, categoria, descricao) {
   const form = document.createElement("form")
   form.className = "form__product"
   form.innerHTML = `
     <h2 class="product__title">Editar produto</h2>
     <div class="product__area">
-      <input class="product__area__input" type="text" id="nome" value="${product.imagem}" data-imagem required />
+      <input class="product__area__input" type="text" id="nome" value="${imagem}" data-imagem required />
       <span>URL da imagem</span>
     </div>
     <div class="product__area">
-      <input class="product__area__input" type="text" id="nome" value="${product.categoria}" data-categoria required />
+      <input class="product__area__input" type="text" id="nome" value="${categoria}" data-categoria required />
       <span>Categoria</span>
     </div>
     <div class="product__area">
-      <input class="product__area__input" type="text" id="nome" value="${product.titulo}" data-titulo required />
+      <input class="product__area__input" type="text" id="nome" value="${titulo}" data-titulo required />
       <span>Nome do produto</span>
     </div>
     <div class="product__area">
-      <input class="product__area__input" type="text" id="nome" value="${product.preco}" data-preco required />
+      <input class="product__area__input" type="text" id="nome" value="${preco}" data-preco required />
       <span>Preço do produto</span>
     </div>
     <div class="product__area">
@@ -36,29 +35,35 @@ async function getProduct() {
         name="mensagem"
         data-descricao
         required
-      >${product.descricao}</textarea>
+      >${descricao}</textarea>
       <span>Descrição do produto</span>
     </div>
     <button class="product__btn" id="product__btn" type="submit">Editar produto</button>
   `
-
-  formData.appendChild(form)
-
-  console.table(product);
-
-  form.addEventListener("submit", async (event) => {
-    event.preventDefault();
-
-    const titulo = document.querySelector("[data-titulo]").value;
-    const preco = document.querySelector("[data-preco]").value;
-    const imagem = document.querySelector("[data-imagem]").value;
-    const categoria = document.querySelector("[data-categoria]").value;
-    const descricao = document.querySelector("[data-descricao]").value;
-
-    await connectApi.editProducts(idUrl, titulo, preco, imagem, categoria, descricao);
-    window.location.href = "../../pages/all-products.html";
-  });
-
 }
 
-getProduct();
+// Edita o produto
+async function editProduct(event) {
+  event.preventDefault();
+
+  const titulo = document.querySelector("[data-titulo]").value;
+  const preco = document.querySelector("[data-preco]").value;
+  const imagem = document.querySelector("[data-imagem]").value;
+  const categoria = document.querySelector("[data-categoria]").value;
+  const descricao = document.querySelector("[data-descricao]").value;
+
+  // Envia os dados para editar o produto
+  await connectApi.editProducts(idUrl, titulo, preco, imagem, categoria, descricao);
+  window.location.href = "../../pages/all-products.html";
+}
+
+// Carrega os dados e edita o produto
+async function loadProduct() {
+  const product = await connectApi.selectById(idUrl);
+  
+  formData.appendChild(createForm(product.titulo, product.preco, product.imagem, product.categoria, product.descricao))
+
+  form.addEventListener("submit", async (event) => editProduct(event));
+}
+
+loadProduct();
